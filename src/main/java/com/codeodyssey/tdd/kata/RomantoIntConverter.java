90d127fb639bf4345d2a2099facdbb92328f0b19
Hiran <hiranhari6@gmail.com>
2021-01-03 15:39:03 +0800

    Code Refactored

--- src/main/java/com/codeodyssey/tdd/kata/RomantoIntConverter.java
+++ src/main/java/com/codeodyssey/tdd/kata/RomantoIntConverter.java
@@ -1,41 +1,26 @@
 
 package com.codeodyssey.tdd.kata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author codeodyssey
  *
  */
 public class RomantoIntConverter {
 
-	public static Integer convertRomantoInt(String input) {
+	public static Integer convertRomantoInt(String input,Map<Character, Integer> romanNumeralsMap) {
 
 		char[] inputRomanSymbol = input.toCharArray();
 		int size = inputRomanSymbol.length;
 		int result = 0;
 		for (int i = 0; i < size; i++) {
-			int numValue = getIntValue(inputRomanSymbol[i]);
+		
+			int numValue = 	romanNumeralsMap.get(inputRomanSymbol[i]);
 			result += numValue;
 		}
 		return result;
-	}
-
-	private static int getIntValue(char inputRoman) {
-		if (inputRoman == 'I') {
-			return 1;
-		}
-		if (inputRoman == 'V')
-			return 5;
-		if (inputRoman == 'X')
-			return 10;
-		if (inputRoman == 'L')
-			return 50;
-		if (inputRoman == 'C')
-			return 100;
-		if (inputRoman == 'D')
-			return 500;
-		if (inputRoman == 'M')
-			return 1000;
-		return -1;
 	}
 
 }
--- src/test/java/com/codeodyssey/tdd/kata/RomantoIntConverterTests.java
+++ src/test/java/com/codeodyssey/tdd/kata/RomantoIntConverterTests.java
@@ -1,36 +1,50 @@
 package com.codeodyssey.tdd.kata;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RomantoIntConverterTests {
+	static Map<Character, Integer> chartoIntMap = new HashMap<>();
 
 	@Test
 	public void convertRomanItoInt() {
-		assertEquals(1, RomantoIntConverter.convertRomantoInt("I"));
+		assertEquals(1, RomantoIntConverter.convertRomantoInt("I", chartoIntMap));
 	}
 
 	@Test
 	public void convertRomanIItoInt() {
-		assertEquals(2, RomantoIntConverter.convertRomantoInt("II"));
+		assertEquals(2, RomantoIntConverter.convertRomantoInt("II",chartoIntMap));
 	}
 
 	@Test
 	public void convertRomanIIItoInt() {
-		assertEquals(3, RomantoIntConverter.convertRomantoInt("III"));
+		assertEquals(3, RomantoIntConverter.convertRomantoInt("III",chartoIntMap));
 	}
 
 	@Test
 	public void convertRomanVtoInt() {
-		assertEquals(5, RomantoIntConverter.convertRomantoInt("V"));
+		assertEquals(5, RomantoIntConverter.convertRomantoInt("V",chartoIntMap));
 	}
-	
+
 	@Test
 	public void convertRomanXtoInt() {
-		assertEquals(10, RomantoIntConverter.convertRomantoInt("X"));
+		assertEquals(10, RomantoIntConverter.convertRomantoInt("X",chartoIntMap));
+	}
+
+	@BeforeAll
+	public static void setup() {
+		chartoIntMap.put('I', 1);
+		chartoIntMap.put('V', 5);
+		chartoIntMap.put('X', 10);
+		chartoIntMap.put('L', 50);
+		chartoIntMap.put('C', 100);
+		chartoIntMap.put('D', 500);
+		chartoIntMap.put('M', 1000);
 	}
 
 }
