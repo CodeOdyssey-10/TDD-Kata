939353a0c8f4ebdfb23a9e1b7021556f4c5546ca
Hiran <hiranhari6@gmail.com>
2021-01-03 12:52:05 +0800

    Initial Commit

---
+++ src/main/java/com/codeodyssey/tdd/kata/TddKataApplication.java
@@ -0,0 +1,13 @@
+package com.codeodyssey.tdd.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddKataApplication.class, args);
+	}
+
+}
